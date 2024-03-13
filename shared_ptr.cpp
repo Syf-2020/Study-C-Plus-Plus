23d6ec39065a40fd3c22511af95c98aff2819552
Syf-2020 <1612196560@qq.com>
2024-03-13 12:48:54 +0800

    手搓智能指针shared_ptr。（非模板）

--- shared_ptr.cpp
+++ shared_ptr.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 
-#define Begin(x) \
-    namespace x  \
-    {
-#define End(x) }
 class A
 {
 public:
@@ -17,82 +13,68 @@ public:
     {
         std::cout << "constructor" << std::endl;
     }
-    ~A()
-    {
-        std::cout << "destructor" << std::endl;
-    }
+    ~A() { std::cout << "destructor" << std::endl; }
 };
 
-Begin(my) class ptr_date
+namespace my
 {
-public:
-    ptr_date(A *ptr, int *cnt = nullptr)
-        : ptr(ptr), cnt(cnt)
+    class ptr_date
     {
-        if (this->cnt == nullptr)
-            this->cnt = new int(1);
+    public:
+        ptr_date(A *ptr, int *cnt = nullptr) : ptr(ptr), cnt(cnt)
+        {
+            if (this->cnt == nullptr)
+                this->cnt = new int(1);
+        };
+        void increase_one() const { *cnt += 1; }
+        void decrease_one() const
+        {
+            *cnt -= 1;
+            if (*cnt == 0)
+                delete ptr;
+        }
+        bool operator==(const ptr_date &p) const
+        {
+            return p.ptr == ptr && p.cnt == cnt;
+        }
+        ~ptr_date()
+        {
+            decrease_one();
+            if (*cnt == 0)
+                delete cnt;
+        }
+        A *ptr;
+        int *cnt;
     };
-    void increase_one() const
+    class shared_ptr
     {
-        *cnt += 1;
-    }
-    void decrease_one() const
-    {
-        *cnt -= 1;
-        if (*cnt == 0)
-            delete ptr;
-    }
-    bool operator==(const ptr_date &p) const
-    {
-        return p.ptr == ptr && p.cnt == cnt;
-    }
-    ~ptr_date()
-    {
-        decrease_one();
-        if (*cnt == 0)
-            delete cnt;
-    }
-    A *ptr;
-    int *cnt;
-};
-class shared_ptr
-{
-public:
-    shared_ptr(A *ptr) : p(ptr) {}
+    public:
+        shared_ptr(A *ptr) : p(ptr) {}
 
-    shared_ptr(const shared_ptr &ths) : p(ths.p)
-    {
-        p.increase_one();
-    }
-    A *operator->() const { return p.ptr; }
+        shared_ptr(const shared_ptr &ths) : p(ths.p) { p.increase_one(); }
+        A *operator->() const { return p.ptr; }
 
-    A &operator*() const { return *p.ptr; }
+        A &operator*() const { return *p.ptr; }
 
-    shared_ptr &operator=(const shared_ptr &obj)
-    {
-        if (obj.p == p)
+        shared_ptr &operator=(const shared_ptr &obj)
+        {
+            if (obj.p == p)
+                return *this;
+            p.decrease_one();
+            p = obj.p;
+            p.increase_one();
             return *this;
-        p.decrease_one();
-        p = obj.p;
-        p.increase_one();
-        return *this;
-    }
-    int use_count()
-    {
-        return *p.cnt;
-    }
-    ~shared_ptr() = default;
+        }
+        int use_count() { return *p.cnt; }
+        ~shared_ptr() = default;
 
-private:
-    ptr_date p;
-};
-shared_ptr make_shared()
-{
-    return shared_ptr(new A());
+    private:
+        ptr_date p;
+    };
+    shared_ptr make_shared() { return shared_ptr(new A()); }
 }
-End(my)
 
-    int main()
+int main()
 {
     {
         my::shared_ptr p = my::make_shared();
